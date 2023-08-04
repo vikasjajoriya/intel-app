ffa7a4cfea6ba5f1ccf5f5fa9c16972d02b3154b
UrCodeBuddy <114049613+vikasjajoriya@users.noreply.github.com>
2023-08-04 11:50:33 +0530

    changes

---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.css"
+const Button = () => {
+  return (
+    <button className='btn3'>
+      Join the waitlist
+    </button>
+  )
+}
+
+export default Button
